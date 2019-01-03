95e01e5706818ced9bf0ebc4cbb59e0415e6d6bc
archana sudani <archanasudani@gmail.com>
2019-01-03 19:42:10 +0000

    My First Commit

---
+++ src/loveProgramming/MyMain.java
@@ -0,0 +1,34 @@
+package loveProgramming;
+
+public class MyMain {
+
+        public static void main(String[] args) {
+
+            Person p1 = new Person();
+            p1.setFirstName("Rahul");
+            p1.setLastName("Gandhi");
+            p1.setAge(15);
+
+            Person p2 = new Person();
+            p2.setFirstName("Archana");
+            p2.setLastName("Sudani");
+            p2.setAge(30);
+
+            System.out.println( "My Name is " + p1.getFirstName() + " " + p1.getLastName()+".");
+            System.out.println( "I am " + p1.getAge()+ " Years old.");
+            System.out.println( "I want to be your PM in 2019.");
+
+            System.out.println( "My Name is " + p2.getFirstName() + " " + p2.getLastName()+".");
+            System.out.println( "I am " + p2.getAge()+ " Years old.");
+            System.out.println( "I want to be your PM in 2019.");
+
+
+
+        }
+
+
+
+
+    }
+
+
